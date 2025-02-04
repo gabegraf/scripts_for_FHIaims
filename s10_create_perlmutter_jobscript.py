b3375f8fe893535b02c74d9db00ae12f4b36ea5c
Yepthatscoool <gabegraf@gmail.com>
2025-02-04 14:45:09 -0500

    Update FHI-aims version

--- s10_create_perlmutter_jobscript.py
+++ s10_create_perlmutter_jobscript.py
@@ -26,7 +26,7 @@ export I_MPI_TUNING_BIN=/opt/intel/oneapi/mpi/2021.6.0/etc/tuning_generic_shm-of
 # Need library for srun to function
 export I_MPI_PMI_LIBRARY=/usr/lib/shifter/mpich-2.2/dep/libpmi.so.0
 
-srun -n ${SLURM_NTASKS} ~/fhi-aims.240920/build/aims.240920.scalapack.mpi.x > aims.out
+srun -n ${SLURM_NTASKS} ~/FHIaims/build/aims.250131.scalapack.mpi.x > aims.out
 """
 
 # Write the content to the file
