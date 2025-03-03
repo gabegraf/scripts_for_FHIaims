5c5f10386889cdfc319ad5ccef799c283a7b2149
Gabriel Graf <gjg20@dcc-login-01.oit.duke.edu>
2025-03-03 14:36:02 -0500

    Minor revisions to s23

--- s23_extract_energy.py
+++ s23_extract_energy.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-def extract_energy(filename)
+def extract_energy(filename):
     import os 
     import re
 
     energy_pattern = r"\|\s+Total energy of the DFT / Hartree-Fock s\.c\.f\. calculation\s+:\s+(-?\d+\.\d+)\s+eV"
 
-        if os.path.exists(filename):
-            with open(filename, "r") as file:
-                for line in file:
-                    match = re.search(energy_pattern, line)
-                    if match:
-                        energy_value = float(match.group(1))
-                        break
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            for line in file:
+                match = re.search(energy_pattern, line)
+                if match:
+                    energy_value = float(match.group(1))
+                    break
     return energy_value
 
 
