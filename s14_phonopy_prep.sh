cee03dc3edf78fe48f8f2f5106e5ae700cfc7dee
Gabriel Graf <gjg20@timewarp-02.egr.duke.edu>
2025-03-21 13:20:48 +0000

    Order s21 output and add "be_nice" to s14
    The outputs of the s21 script are now ordered,
    and s12 has an additional flag --be_nice, which
    runs all the phonon calculations on lower priority
    with SLURM.

--- s14_phonopy_prep.sh
+++ s14_phonopy_prep.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-for file in geometry.in-*; do 
-	number=$(echo "$file" | grep -oP '(?<=geometry.in-)\d+')
+# Check if the --be_nice flag is passed
+be_nice=false
+for arg in "$@"; do
+    if [[ "$arg" == "--be_nice" ]]; then
+        be_nice=true
+        break
+    fi
+done
 
-	folder="${number}-displacement"
+for file in geometry.in-*; do
+    number=$(echo "$file" | grep -oP '(?<=geometry.in-)\d+')
 
-	mkdir -p "$folder"
-	mv "$file" "$folder/geometry.in"
+    folder="${number}-displacement"
 
-	cp jobscript.sh control.in "$folder/"
+    mkdir -p "$folder"
+    mv "$file" "$folder/geometry.in"
 
-	cd "$folder" && sbatch jobscript.sh && cd ../
+    cp jobscript.sh control.in "$folder/"
+
+    # If --be_nice flag is passed, run the job with nice 100
+    if $be_nice; then
+        cd "$folder" && sbatch --nice=100 jobscript.sh && cd ../
+    else
+        cd "$folder" && sbatch jobscript.sh && cd ../
+    fi
 done
 
 echo "All jobs have been submitted!"
+
--- s21_am_I_done_yet.sh
+++ s21_am_I_done_yet.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Find all directories ending with '-displacement'
-for dir in $(find . -type d -name "*-displacement"); do
-    
+# Find all directories ending with '-displacement' and sort them
+for dir in $(find . -type d -name "*-displacement" | sort); do
+
     # Define the path to aims.out
     aims_out="$dir/aims.out"
-    
+
     # Check if aims.out exists in the directory
     if [[ -f "$aims_out" ]]; then
-        
+
         # Check if "Have a nice day" exists in the file
         if grep -q "Have a nice day" "$aims_out"; then
             echo "Success: 'Have a nice day' found in $aims_out"
