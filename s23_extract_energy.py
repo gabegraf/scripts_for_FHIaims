a195cb518ccbba3698fd8590eeff651b18370eaa
Gabriel Graf <gabegraf@gmail.com>
2025-06-02 13:11:22 +0200

    s23 takes filename arguments

--- s23_extract_energy.py
+++ s23_extract_energy.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-def extract_energy(filename):
-    import os 
-    import re
+import os
+import re
+import sys
 
+def extract_energy(filename):
     energy_pattern = r"\|\s+Total energy of the DFT / Hartree-Fock s\.c\.f\. calculation\s+:\s+(-?\d+\.\d+)\s+eV"
 
     if os.path.exists(filename):
@@ -11,11 +12,17 @@ def extract_energy(filename):
                 match = re.search(energy_pattern, line)
                 if match:
                     energy_value = float(match.group(1))
-                    break
-    return energy_value
-
+                    return energy_value
+    else:
+        print(f"Error: File '{filename}' does not exist.")
+        return None
 
 if __name__ == "__main__":
+    # Use default filename unless an argument is provided
     filename = "aims.out"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+
     energy = extract_energy(filename)
-    print(energy)
+    if energy is not None:
+        print(energy)
