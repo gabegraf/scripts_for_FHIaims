a1fcdb2fe4bbb197b3cde77d0539a78caddd4e69
Gabriel Graf <gabriel.graf@duke.edu>
2025-11-25 16:08:33 -0500

    Add s47 no_primitive flag

--- s47_symmetry_refinement.py
+++ s47_symmetry_refinement.py
@@ -9,18 +9,20 @@ import click
 @click.command()
 @click.option("--tol", default=0.001, help="Tolerance for initial space group analysis")
 @click.option("--output", default="geometry_refined.in", help="Name of the output file")
+@click.option("--no_primitive", is_flag=True, help="Do not convert to primitive cell")
 
-def symmetrization(output, tol):
+def symmetrization(output, tol, no_primitive):
   atoms = read('geometry.in', format='aims')
   structure = AseAtomsAdaptor.get_structure(atoms)
   sga = SpacegroupAnalyzer(structure, tol)
   print(f"Space Group Found! It's {sga.get_space_group_number()} ({sga.get_space_group_symbol()})")
-
+  
   structure_2 = sga.get_refined_structure()
   sga2 = SpacegroupAnalyzer(structure_2, tol)
-  structure_3 = sga2.find_primitive()
+  if not no_primitive:
+    structure_2 = sga2.get_primitive_standard_structure()
 
-  atoms = AseAtomsAdaptor.get_atoms(structure_3)
+  atoms = AseAtomsAdaptor.get_atoms(structure_2)
   atoms.center()
   atoms.wrap()
 
